9600574c0dfdfaa8e0dc34ca09813e217bb56e51
karazzi <fsteenfeldt@gmail.com>
2021-10-11 23:45:50 +0200

    Uge 5 nedarvning

---
+++ Uge 5/Nedarvning/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Nedarvning.Models;
+
+namespace Nedarvning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parent parent = new Parent();
+            Child child = new Child();
+
+            parent.Display();
+            child.Display();
+        }
+    }
+}
